3a014919857648ceb761927456ab329659de48be
Han Trinh <hantn16@gmail.com>
2018-02-02 22:46:27 +0700

    Add form for messages and show incoming mes

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,30 @@
 var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server!!!');
-    // socket.emit('createEmail', {
-    //     to: 'bang90@gmail.com',
-    //     text: "I'm Bang. Nice to meet you too"
-    // });
-    // socket.emit('createMessage', {
-    //     to: 'hantn16@gmail.com',
-    //     text: "I'm fine, tks"
-    // });
 });
 socket.on('disconnect', function () {
     console.log('Disconnected form server!');
 });
-socket.on('newEmail', function (email) {
-    console.log('New email:', email);
-});
 
 socket.on('newMessage', function (message) {
     console.log('New message:', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
+
+});
+// socket.emit('createMessage', {
+//     from: 'Han Trinh',
+//     text: "I'm fine, tks"
+// }, function(data) {
+//     console.log('Got it', data);
+// });
+$('#message-form').on('submit',function(event){
+    event.preventDefault();
+    socket.emit('createMessage',{
+        from: 'User',
+        text: $('[name=message]').val()
+    },function() {
+
+    });
 });
