eaef9a255060a12650ffeee4cd47d440758c9e9d
Han Trinh <hantn16@gmail.com>
2018-02-04 16:57:46 +0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,21 @@ var socket = io();
 var locationButton = jQuery('#send-location');
 var messageTextbox = $('[name=message]');
 
+function scrollToBottom() {
+    //Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server!!!');
 });
@@ -14,26 +29,27 @@ socket.on('newMessage', function (message) {
     console.log('New message:', message);
     var formattedTime = moment(message.createdAt).format('hh:mm a');
     var template = $('#message-template').html();
-    var html = Mustache.render(template,{
+    var html = Mustache.render(template, {
         text: message.text,
         from: message.from,
         createdAt: formattedTime
     });
     $('#messages').append(html);
-
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
     console.log('New message:', message);
     var formattedTime = moment(message.createdAt).format('hh:mm a');
     var template = $('#location-message-template').html();
-    var html = Mustache.render(template,{
+    var html = Mustache.render(template, {
         text: message.text,
         from: message.from,
         url: message.url,
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', function (event) {
@@ -49,7 +65,7 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser')
     }
-    locationButton.attr('disabled','disabled').text('Sending location...');
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function (position) {
         socket.emit('createLocationMessage', {
             lat: position.coords.latitude,
